958b7517dc5a9aa42337624e313599a3ee1e2514
Apprentice <apprentice@devbootcamp.com>
2013-09-02 11:20:19 -0700

    add index migration for karmapoints

--- db/migrate/20130902181704_add_indexes_to_karma_points.rb
+++ db/migrate/20130902181704_add_indexes_to_karma_points.rb
@@ -1,8 +1,6 @@
 class AddIndexesToKarmaPoints < ActiveRecord::Migration
   def change
-    add_column :karma_points, :value, :string
     add_index :karma_points, :value
-    add_column :karma_points, :user_id, :string
     add_index :karma_points, :user_id
   end
 end
