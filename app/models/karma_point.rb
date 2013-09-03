47a6af9aff96556b23aa9e78a9006abf91b980a9
Apprentice <apprentice@devbootcamp.com>
2013-09-02 20:04:19 -0700

    add karma speed methods'

--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -5,4 +5,27 @@ class KarmaPoint < ActiveRecord::Base
   validates :user, :presence => true
   validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   validates :label, :presence => true
+
+  before_create :add_karma
+  before_destroy :remove_karma
+  before_save :modify_karma
+
+  def add_karma
+    self.user.total_karma += value
+    self.user.save
+  end
+
+  def remove_karma
+    self.user.total_karma -= value
+    self.user.save
+  end
+
+  def modify_karma
+    if (KarmaPoint.find(id) rescue nil)
+      unless KarmaPoint.find(id).value == value
+        self.user.total_karma = user.total_karma - KarmaPoint.find(id).value + value
+        self.user.save
+      end
+    end
+  end  
 end
---
+++ db/build.rb
@@ -0,0 +1,3 @@
+
+
+User.build_karma
